e2f381851f981902d0129e82eb1f54f6f0fff504
DESKTOP-27J6S69\Corva <corvaeoboro@gmail.com>
2024-07-29 13:30:06 -0700

    Added z_blender_GEN_vfx_slash.py script.

---
+++ addon/z_blender_GEN_vfx_slash.py
@@ -0,0 +1,120 @@
+bl_info = {
+    "name": "Parabola Slash Mesh Generator",
+    "category": "ZENV",
+    "author": "CorvaeOboro",
+    "version": (1, 0),
+    "blender": (2, 93, 0),
+    "location": "View3D > Tool",
+    "description": "Parabola Slash Mesh Generator"
+}
+
+import bpy
+from bpy.types import Operator, Panel, PropertyGroup
+from bpy.props import FloatProperty, IntProperty, PointerProperty
+
+def create_parabola_mesh(context, angle, rotation, length, curve_resolution):
+    # Create a new curve object
+    curve_data = bpy.data.curves.new('ParabolaCurve', type='CURVE')
+    curve_data.dimensions = '3D'
+    spline = curve_data.splines.new('BEZIER')
+    spline.bezier_points.add(2)
+    
+    # Set points of the Bézier curve
+    spline.bezier_points[0].co = (0, 0, 0)
+    spline.bezier_points[1].co = (length / 2, length * angle, 0)
+    spline.bezier_points[2].co = (length, 0, 0)
+    
+    for point in spline.bezier_points:
+        point.handle_right_type = 'AUTO'
+        point.handle_left_type = 'AUTO'
+    
+    # Create an object with the curve data
+    curve_object = bpy.data.objects.new('ParabolaCurve', curve_data)
+    bpy.context.collection.objects.link(curve_object)
+    
+    # Convert curve to mesh
+    bpy.context.view_layer.objects.active = curve_object
+    bpy.ops.object.convert(target='MESH')
+
+    # Apply rotation
+    curve_object.rotation_euler = (0, 0, rotation)
+
+    # Add UV mapping
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.uv.unwrap()
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    return curve_object
+
+class ZENVProperties(PropertyGroup):
+    angle: FloatProperty(
+        name="Angle",
+        description="Angle of the parabola",
+        default=0.5,
+        min=-2.0,
+        max=2.0
+    )
+    rotation: FloatProperty(
+        name="Rotation",
+        description="Rotation of the parabola",
+        default=0.0,
+        min=-3.14,
+        max=3.14
+    )
+    length: FloatProperty(
+        name="Length",
+        description="Length of the parabola",
+        default=2.0,
+        min=0.1,
+        max=10.0
+    )
+    curve_resolution: IntProperty(
+        name="Curve Resolution",
+        description="Resolution of the curve",
+        default=12,
+        min=3,
+        max=64
+    )
+
+class OBJECT_OT_add_parabola_mesh(Operator):
+    bl_idname = "object.add_parabola_mesh"
+    bl_label = "Add Parabola Mesh"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
+        props = context.scene.zenv_properties
+        create_parabola_mesh(context, props.angle, props.rotation, props.length, props.curve_resolution)
+        return {'FINISHED'}
+
+class ZENV_PT_panel(Panel):
+    bl_idname = "ZENV_PT_panel"
+    bl_label = "Parabola Slash Mesh Generator"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'ZENV'
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        props = scene.zenv_properties
+        
+        layout.prop(props, "angle")
+        layout.prop(props, "rotation")
+        layout.prop(props, "length")
+        layout.prop(props, "curve_resolution")
+        layout.operator("object.add_parabola_mesh")
+
+def register():
+    bpy.utils.register_class(ZENVProperties)
+    bpy.types.Scene.zenv_properties = PointerProperty(type=ZENVProperties)
+    bpy.utils.register_class(OBJECT_OT_add_parabola_mesh)
+    bpy.utils.register_class(ZENV_PT_panel)
+
+def unregister():
+    bpy.utils.unregister_class(ZENVProperties)
+    del bpy.types.Scene.zenv_properties
+    bpy.utils.unregister_class(OBJECT_OT_add_parabola_mesh)
+    bpy.utils.unregister_class(ZENV_PT_panel)
+
+if __name__ == "__main__":
+    register()
